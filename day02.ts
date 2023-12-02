bfe6df304d07b96b0f9a2e5f1bebe3293bd32489
mike castleman <m@mlcastle.net>
2023-12-02 00:19:34 -0500

    clean up

--- day02.ts
+++ day02.ts
@@ -1,9 +1,9 @@
 import { getNums, readSplit, show, sumBy } from './util.ts';
 
 interface Round {
-  red?: number;
-  green?: number;
-  blue?: number;
+  red: number;
+  green: number;
+  blue: number;
 }
 
 interface Game {
@@ -11,15 +11,16 @@ interface Game {
   rounds: Round[];
 }
 
-const parseR = (r: string): Round => {
-  const entries = r.split(', ');
-  return Object.fromEntries(
-    entries.map((s) => {
-      const [n, key] = s.split(' ');
-      return [key, Number(n)];
-    })
-  ) as Round;
-};
+const parseR = (r: string): Round =>
+  Object.assign(
+    { red: 0, green: 0, blue: 0 },
+    Object.fromEntries(
+      r.split(', ').map((s) => {
+        const [n, key] = s.split(' ');
+        return [key, Number(n)];
+      })
+    )
+  );
 
 const parse = (x: string): Game => {
   const [idpart, ...parts] = x.split(/[:;] /);
@@ -31,8 +32,7 @@ const input = (await readSplit(2)).map(parse);
 
 const okay = ({ rounds }: Game) =>
   rounds.every(
-    ({ red, green, blue }) =>
-      (red ?? 0) <= 12 && (green ?? 0) <= 13 && (blue ?? 0) <= 14
+    ({ red, green, blue }) => red <= 12 && green <= 13 && blue <= 14
   );
 
 await show(sumBy(input.filter(okay), ({ id }) => id));
@@ -40,16 +40,16 @@ await show(sumBy(input.filter(okay), ({ id }) => id));
 const maxG = (rs: readonly Round[]): Round =>
   rs.reduce(
     (a, b) => ({
-      red: Math.max(a.red ?? 0, b.red ?? 0),
-      green: Math.max(a.green ?? 0, b.green ?? 0),
-      blue: Math.max(a.blue ?? 0, b.blue ?? 0),
+      red: Math.max(a.red, b.red),
+      green: Math.max(a.green, b.green),
+      blue: Math.max(a.blue, b.blue),
     }),
     { red: 0, green: 0, blue: 0 }
   );
 
 const power = ({ rounds }: Game) => {
   const m = maxG(rounds);
-  return m.red! * m.green! * m.blue!;
+  return m.red * m.green * m.blue;
 };
 
 await show(sumBy(input, power));
